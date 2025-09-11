a4f951cb83f2970f8a1bae2bbf467c577db0018b
pruthviraj7714 <marganepruthviraj@gmail.com>
2025-09-11 12:01:52 +0530

    credentials post endpoint added

--- apps/server/routes/credential.routes.ts
+++ apps/server/routes/credential.routes.ts
@@ -1,10 +1,41 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/authMiddleware";
+import { CredentialsSchema } from "@repo/common";
+import prisma from "@repo/db";
 
 const credentialRouter = Router();
 
-credentialRouter.post("/", authMiddleware, (req, res) => {
+credentialRouter.post("/", authMiddleware, async (req, res) => {
   try {
+    const userId = req.userId!;
+
+    const parsed = CredentialsSchema.safeParse(req.body);
+
+    if (parsed.error) {
+      const formatted = parsed.error.flatten();
+
+      res.status(400).json({
+        message: "Invalid Inputs",
+        fieldErrors: formatted.fieldErrors,
+        formErrors: formatted.formErrors,
+      });
+      return;
+    }
+
+    const { data: credentialsData, platform, title } = parsed.data;
+
+    await prisma.credentials.create({
+      data: {
+        data: credentialsData,
+        platform,
+        title,
+        userId,
+      },
+    });
+
+    res.status(201).json({
+      message: "Credentials Successfully Stored",
+    });
   } catch (error) {
     res.status(500).json({
       message: "Internal Server Error",
@@ -14,6 +45,7 @@ credentialRouter.post("/", authMiddleware, (req, res) => {
 
 credentialRouter.delete("/", authMiddleware, (req, res) => {
   try {
+    const userId = req.userId;
   } catch (error) {
     res.status(500).json({
       message: "Internal Server Error",
