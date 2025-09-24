3a16232336b45268b17c08fdd6372eedcc17484f
pruthviraj7714 <marganepruthviraj@gmail.com>
2025-09-24 13:05:22 +0530

    DB schema fixes

---
+++ packages/db/prisma/migrations/20250924065239_removed_title_creds/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `Credentials` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Credentials" DROP COLUMN "title";
